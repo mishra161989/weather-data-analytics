7d1b17bd6d8644fef11f1715e800823e4f04bb9b
mishra161989 <57952912+mishra161989@users.noreply.github.com>
2024-04-13 12:51:17 +0530

    added weather analytics components for cicd

---
+++ dags/transform_redshift_load.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.providers.amazon.aws.operators.glue import GlueJobOperator
+from airflow.sensors.external_task_sensor import ExternalTaskSensor
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 4, 12),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG('transform_redshift_dag', default_args=default_args, schedule_interval="@once",catchup=False)
+
+
+# Define the Glue Job
+transform_task = GlueJobOperator(
+    task_id='transform_task',
+    job_name='glue_transform_task',
+    script_location='s3://aws-glue-assets-992382818239-us-east-2/scripts/weather_data_ingestion.py',
+    s3_bucket='s3://aws-glue-assets-992382818239-us-east-2',  # S3 bucket where logs and local etl script will be uploaded
+    aws_conn_id='aws_default',  # You'll need to set up an AWS connection in Airflow
+    region_name="us-east-2",
+    iam_role_name='AWSGlueServiceRole-Glue-role',
+    create_job_kwargs ={"GlueVersion": "4.0", "NumberOfWorkers": 2, "WorkerType": "G.1X", "Connections":{"Connections":["redshift_connection"]},},
+    dag=dag,
+)
